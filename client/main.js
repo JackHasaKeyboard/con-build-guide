0a40f5c15d5f55254e1ada65a5080919b399c074
jackhasakeyboard <jackhasakeyboard@gmail.com>
2017-08-17 20:47:03 -0600

    Remove redundant sourcing of YouTube API script

--- client/main.js
+++ client/main.js
@@ -91,11 +91,6 @@ $(document).ready(function() {
 		});
 	});
 
-	var tag = document.createElement('script');
-	tag.src = "https://www.youtube.com/player_api";
-	var firstScriptTag = document.getElementsByTagName('script')[0];
-	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
 	// player
 	window.onYouTubePlayerAPIReady = function() {
 		player = new YT.Player('player', {
